f4b346c9fa3b7923e7081a191852e88b363b7d1d
Miles Johnson <mileswjohnson@gmail.com>
2014-10-29 21:09:38 -0700

    Added a robust annotation system based on Hack attributes.

---
+++ Exception/MissingObjectException.hh
@@ -0,0 +1,17 @@
+<?hh // strict
+/**
+ * @copyright   2010-2013, The Titon Project
+ * @license     http://opensource.org/licenses/bsd-license.php
+ * @link        http://titon.io
+ */
+
+namespace Titon\Utility\Exception;
+
+/**
+ * Exception thrown when an object doesn't exist in the registry.
+ *
+ * @package Titon\Common\Exception
+ */
+class MissingObjectException extends \OutOfRangeException {
+
+}
